c854b897b623cee3b76a84378323e5e513c2cef1
swapnil222359 <swapsingale@gmail.com>
2018-01-09 00:07:02 +0530

    initial

---
+++ src/main/java/com/repo/scdiscoveryserver/ScDiscoveryServerApplication.java
@@ -0,0 +1,12 @@
+package com.repo.scdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScDiscoveryServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/repo/scdiscoveryserver/ScDiscoveryServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.repo.scdiscoveryserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScDiscoveryServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
