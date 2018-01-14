fc08675eab7268f61fae6636d8d47c799d455028
swapnil222359 <swapsingale@gmail.com>
2018-01-14 15:46:44 +0530

    added eureka discovery server configurations

--- src/main/java/com/repo/scdiscoveryserver/ScDiscoveryServerApplication.java
+++ src/main/java/com/repo/scdiscoveryserver/ScDiscoveryServerApplication.java
@@ -2,7 +2,9 @@ package com.repo.scdiscoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ScDiscoveryServerApplication {
 
